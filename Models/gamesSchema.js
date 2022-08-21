c9a42d51078bcf97ae5b054a9346f453d013945e
Misha <neibips@gmail.com>
2022-08-22 01:29:08 +0500

    make it better

--- Models/gamesSchema.js
+++ Models/gamesSchema.js
@@ -3,7 +3,8 @@ const {Schema, model} = require('mongoose')
 const GamesSchema = new Schema({
     walletAdress: String,
     result: Boolean,
-    amount: Number
+    amount: Number,
+    timeStamp: Date
 })
 
 module.exports =  model('Games', GamesSchema);
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,7 @@ const Games = require('../Models/gamesSchema')
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try{
-    const games = await Games.find().sort().select('walletAdress result amount').limit(() => {
-
-    }).lean()
+    const games = await Games.find().sort().select('walletAdress result amount timeStamp').limit().lean()
     games.reverse()
     res.send(games);
   }catch (e) {
@@ -17,10 +15,12 @@ router.get('/', async function(req, res, next) {
 
 router.post('/flip', async (req, res, next) => {
   const {walletAdress, result, amount} = req.body
+  const timeStamp = Date.now().toLocaleString()
   await Games.create({
     walletAdress,
     result,
-    amount
+    amount,
+    timeStamp
   })
   res.redirect('http://localhost:3000/flip')
 })
