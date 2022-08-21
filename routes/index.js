c773fb4335699cbd3e7f11f7f817c5f6938767f8
Misha <neibips@gmail.com>
2022-08-22 01:40:18 +0500

    make it better

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,6 @@ const Games = require('../Models/gamesSchema')
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try{
-    const timeStamp = Date.now()
-    await Games.create({
-      walletAdress: '21',
-      result: true,
-      amount: 2,
-      timeStamp
-    })
     const games = await Games.find().sort().select('walletAdress result amount timeStamp').limit().lean()
     games.reverse()
     res.send(games);
@@ -22,7 +15,7 @@ router.get('/', async function(req, res, next) {
 
 router.post('/flip', async (req, res, next) => {
   const {walletAdress, result, amount} = req.body
-  const timeStamp = Date.now().toLocaleString()
+  const timeStamp = Date.now()
   await Games.create({
     walletAdress,
     result,
