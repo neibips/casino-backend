6e3327473502e8c9ca73d6fdeb69eae7b87f80e1
Misha <neibips@gmail.com>
2022-08-22 18:36:56 +0500

    bro

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -3,6 +3,7 @@ const {model, Schema} = require('mongoose')
 const userSchema = new Schema({
     wallet: {
         type: String,
+        unique: true
     },
     balance: Number,
     transactions: [String],
